c481925f8b0365ec8a237f5ed047cc705e090d53
0994486@gmail.com <0994486@gmail.com>
2020-10-14 20:11:49 +0300

    Добавляет сервер

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ express()
 // .set(`views`, path.join(__dirname, `views`))
 // .set(`view engine`, `ejs`)
 .get(`/`, (req, res) => {
+  res.append(`Access-Control-Allow-Origin`, [`*`]);
   res.send(`rukivbruki`);
 })
 .listen(PORT, () => console.log(`Listening on ${PORT}`));
